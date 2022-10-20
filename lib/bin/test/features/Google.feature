48efcde78e50a4ca338073ce39dacaca26520d81
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-10-19 23:44:07 -0500

    Subiendo proyecto al repositorio

---
+++ lib/bin/test/features/Google.feature
@@ -0,0 +1,12 @@
+
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  
+  Scenario: Title of your scenario
+    Given I am on the Google Search page
+    When I enter a search critera
+    And click on the search critera   
+    Then the results match the criteria
+
+
---
+++ lib/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,27 @@
+package steps;
+
+import cucumber.api.java.en.*;
+
+public class GoogleSteps {
+	
+	@Given("^I am on the Google Search page$")
+	public void navigateGoogle() {
+		
+	}
+	
+	@When("^I enter a search critera$")
+	public void enterSearchCriteria() {
+		
+	}
+	
+	@And("^click on the search critera$")
+	public void clickSearchButton() {
+		
+	}
+	
+	@Then("^the results match the criteria$")
+	public void validateResults() {
+		
+	}
+	
+}
