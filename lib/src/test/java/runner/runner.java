1720d8e3a0853bf22c693ba4d0d5e433d0512d43
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-10-21 16:30:04 -0500

    Add reporte basico

--- lib/src/test/java/runner/runner.java
+++ lib/src/test/java/runner/runner.java
@@ -11,6 +11,9 @@ import pages.BasePage;
 @CucumberOptions(features="src/test/resources/features",  
 		glue={"steps"},
 		monochrome = true,
+		 plugin = { "pretty", "html:target/cucumber-reports"},
+		// plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports.json"},
+		// plugin = {"pretty","json:target/cucumber.json"},
 		tags = "@Testing",
 		strict = true)
 
